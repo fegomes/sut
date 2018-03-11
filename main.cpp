82a05012a43b3edabaee008641fd77191516f0ee
fegomes <nandogsilva@hotmail.com>
2018-03-11 19:24:28 -0300

    init commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+//
+// Created by @fegomes on 11/03/18.
+//
+
+#include <spdlog/spdlog.h>
+
+
+
+int main(){
+
+    auto console = spd::stderr_color_st("console");
+    console->info("Starting generate unit test");
+
+
+
+    return 0;
+}
+
